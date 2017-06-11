e8181bcc491975d3bc14a5960b360006ed1a0070
zhangminvip <2107609379@qq.com>
2017-06-11 23:59:51 +0800

    添加NavigationView,添加第二个Activity

--- app/src/main/java/com/example/andriod/materialtest610/MainActivity.java
+++ app/src/main/java/com/example/andriod/materialtest610/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.andriod.materialtest610;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+
+import static android.support.v7.appcompat.R.styleable.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,12 +23,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        NavigationView navView = (NavigationView)findViewById(R.id.nav_view) ;
 
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+        {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+
+        navView.setCheckedItem(R.id.nav_call);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item)
+            {
+
+                switch (item.getItemId()){
+                    case R.id.nav_friends:
+                        Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                        startActivity(intent);
+                        break;
+                    default:
+                }
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+    }
+    @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
         getMenuInflater().inflate(R.menu.toolbar,menu);
 
         return true;
     }
+
+
+    @Override
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+
+            default:
+        }
+
+        return true;
+    }
+
 }
---
+++ app/src/main/java/com/example/andriod/materialtest610/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.andriod.materialtest610;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tom on 2017/6/11.
+ */
+
+public class SecondActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+
+}
