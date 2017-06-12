39f62bd741041dbbad4444cbcaa8faf91a36a062
zhangminvip <2107609379@qq.com>
2017-06-13 00:53:09 +0800

    添加recyclerview.添加一个水果类

---
+++ app/src/main/java/com/example/andriod/materialtest610/Fruit.java
@@ -0,0 +1,26 @@
+package com.example.andriod.materialtest610;
+
+/**
+ * Created by tom on 2017/6/13.
+ */
+
+public class Fruit {
+
+    private String name;
+
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+}
