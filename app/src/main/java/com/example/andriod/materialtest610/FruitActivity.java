e2be0b288bc29e19f6c9273f8734de24dcb4a53c
zhangminvip <2107609379@qq.com>
2017-06-13 23:39:21 +0800

    实现了下拉刷新

---
+++ app/src/main/java/com/example/andriod/materialtest610/FruitActivity.java
@@ -0,0 +1,8 @@
+package com.example.andriod.materialtest610;
+
+/**
+ * Created by tom on 2017/6/13.
+ */
+
+public class FruitActivity {
+}
--- app/src/main/java/com/example/andriod/materialtest610/MainActivity.java
+++ app/src/main/java/com/example/andriod/materialtest610/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private List<Fruit> mFruitList = new ArrayList<>();
     private FruitAdapter mAdapter;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     private Fruit[] mFruits = {
             new Fruit("Apple",R.drawable.apple),
@@ -92,6 +94,8 @@ public class MainActivity extends AppCompatActivity {
         navView = (NavigationView)findViewById(R.id.nav_view) ;
 
         recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        mSwipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipe_refresh);
+
 
     }
 
@@ -145,14 +149,55 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        // receclerview
+
         GridLayoutManager layoutManager = new GridLayoutManager(this,2);
         recyclerView.setLayoutManager(layoutManager);
         mAdapter = new FruitAdapter(mFruitList);
         recyclerView.setAdapter(mAdapter);
 
+        //下拉刷新
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener(){
+            @Override
+            public void onRefresh(){
+                refreshFruits();
+            }
+
+
+        });
+
+
 
 
     }
+
+    //刷新RecyclerView
+    private void refreshFruits(){
+        new Thread(new Runnable(){
+
+            @Override
+            public void run(){
+                try{
+                    Thread.sleep(1500);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+                // 切换回主线程
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        initFruit();
+                        mAdapter.notifyDataSetChanged();
+                        mSwipeRefreshLayout.setRefreshing(false);
+                    }
+                });
+            }
+        }).start();
+    }
+
 
 
     @Override
