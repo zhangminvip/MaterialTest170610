c8209e9fe1a54b77b077c3082149eaf657e3fe0b
zhangminvip <2107609379@qq.com>
2017-06-14 09:10:15 +0800

    实现了状态栏折叠

--- app/src/main/java/com/example/andriod/materialtest610/FruitAdapter.java
+++ app/src/main/java/com/example/andriod/materialtest610/FruitAdapter.java
@@ -1,6 +1,7 @@
 package com.example.andriod.materialtest610;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.Image;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -35,7 +36,24 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
         }
 
         View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item,parent,false);
-        return new ViewHolder(view);
+
+        //处理RecyclerView的点击事件
+
+        final ViewHolder holder = new ViewHolder(view);
+        holder.mCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Intent intent = new Intent(mContext,FruitActivity.class);
+                intent.putExtra(FruitActivity.FRUIT_NAME,fruit.getName());
+                intent.putExtra(FruitActivity.FRUIT_IMAGE_ID,fruit.getImageId());
+                mContext.startActivity(intent);
+            }
+        });
+
+
+        return holder;
     }
 
 
